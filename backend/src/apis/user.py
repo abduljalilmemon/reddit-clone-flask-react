7f3f35f11b6435c4dfe125ff378d9ce3426b3838
Abdul Jalil <abduljaleelmemon3@gmail.com>
2024-01-14 16:20:08 +0500

    add vite react

--- backend/src/apis/user.py
+++ backend/src/apis/user.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, jsonify
-
+# from flask
 user = Blueprint("users", __name__, url_prefix="/user")
-
-
-@user.route("/login", methods=["POST"])
-def user_login():
-    return jsonify({"message": "Invalid credentials"}), 401
 
 
 @user.route("/", methods=["GET"])
 def test():
+    return jsonify({"message": "Invalid credentials"}), 401
+
+
+@user.route("/login", methods=["POST"])
+def login_user():
+    return jsonify({"message": "Invalid credentials"}), 401
+
+
+@user.route("/register", methods=["POST"])
+def register_user():
     return jsonify({"message": "Invalid credentials"}), 401
